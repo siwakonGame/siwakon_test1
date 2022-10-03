cf4e79c81b72da66168beecb4fc41367a8a65553
siwakonGame <112382088+siwakonGame@users.noreply.github.com>
2022-10-03 14:53:29 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("includes/header.php"); ?>
+</head>
+
+<body>
+    <?php include("includes/navbar.php"); ?>
+    <div class="container">
+        <!-- Start -->
+            <h2>Home</h2>
+            <hr>Welcome
+        <!-- End -->
+    </div>
+</body>
+
+</html>
+    <?php include("includes/footer.php"); ?>
